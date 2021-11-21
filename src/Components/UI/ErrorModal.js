1446b10bd6869e2cb72209f917cf90433890ab92
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2021-11-22 00:57:13 +0200

    Working with Portals v.v.v.imp way to write clean code when overlay elements were used (the best practice)

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,12 +1,18 @@
 import React from "react";
+
+import ReactDOM from "react-dom";
+
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 import Button from "./Button";
 
 const ErrorModal = (props) => {
-  return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+  };
+
+  const ModalOverlay = (props) => {
+    return (
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -18,7 +24,25 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
-    </div>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      {/* the following is to port overlay elements from "index.html" which is the best way & practice */}
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
